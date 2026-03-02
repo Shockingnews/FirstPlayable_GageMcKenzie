d735d994dd17432a616fd878cb09cb2fc6281505
Shockingnews <gagemckenzie70@gmail.com>
2026-03-02 10:04:20 -0400

    bug fixes

--- FirstPlayable_GageMcKenzie/NewEnemy.cs
+++ FirstPlayable_GageMcKenzie/NewEnemy.cs
@@ -32,38 +32,42 @@ namespace FirstPlayable_GageMcKenzie
         {
             if (base.maxHealth.health != 0)
             {
-                if(_player.currentPos.x == _enemyPos.x)
+                if(_player.currentPos.x == _enemyPos.x + 3 || _player.currentPos.x == _enemyPos.x - 3)
                 {
+                    if (_player.currentPos.y == _enemyPos.y + 3 || _player.currentPos.y == _enemyPos.y - 3)
+                    {
+                        Random random = new Random();
+                        _randomMovement = random.Next(1, 3);
+                        if (_randomMovement == 1)
+                        {
+                            if (_enemyPos.x > _player.currentPos.x)
+                            {
+                                _enemyPos.x -= 1;
+
+                            }
+                            else if (_enemyPos.x < _player.currentPos.x)
+                            {
+                                _enemyPos.x += 1;
+
+                            }
+                        }
+                        if (_randomMovement == 2)
+                        {
+                            if (_enemyPos.y > _player.currentPos.y)
+                            {
+                                _enemyPos.y -= 1;
+
+                            }
+                            else if (_enemyPos.y < _player.currentPos.y)
+                            {
+                                _enemyPos.y += 1;
+
+                            }
+                        }
+                    }
 
                 }
-                Random random = new Random();
-                _randomMovement = random.Next(1, 3);
-                if (_randomMovement == 1)
-                {
-                    if (_enemyPos.x > _player.currentPos.x)
-                    {
-                        _enemyPos.x -= 1;
-
-                    }
-                    else if (_enemyPos.x < _player.currentPos.x)
-                    {
-                        _enemyPos.x += 1;
-
-                    }
-                }
-                if (_randomMovement == 2)
-                {
-                    if (_enemyPos.y > _player.currentPos.y)
-                    {
-                        _enemyPos.y -= 1;
-
-                    }
-                    else if (_enemyPos.y < _player.currentPos.y)
-                    {
-                        _enemyPos.y += 1;
-
-                    }
-                }
+                
             }
 
 
---
+++ FirstPlayable_GageMcKenzie/ShieldItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstPlayable_GageMcKenzie
+{
+    
+    internal class ShieldItem : Items
+    {
+        int _numOfShieldItems;
+        Player _player;
+        public ShieldItem(Player player, List<int> xPos, List<int> yPos, int numOfShields, char shieldLogo, ConsoleColor shieldColor) : base(player: player, posX: xPos, posY: yPos, numberOfItems: numOfShields, itemLogo: shieldLogo, itemColor: shieldColor)
+        {
+            _numOfShieldItems = numOfShields;
+            _player = player;
+        }
+
+        public override void PlaceItem()
+        {
+            base.PlaceItem();
+            for (int i = 0; i < _numOfShieldItems; i++)
+            {
+                if (base.PickedUp[i] == true)
+                {
+                    _player.maxShield.health += 1;
+                }
+            }
+
+        }
+    }
+
+}
